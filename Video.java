9e8ac2faed5d897e56afc0e227beaf15d202d8d6
Elder Campos <117109983+eldercamposds@users.noreply.github.com>
2022-12-27 23:38:31 -0300

    Create Video.java

---
+++ Video.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetofinaljava;
+
+/**
+ *
+ * @author Casa
+ */
+public final class Video implements AcoesVideo {
+    private String titulo;
+    private int avaliacao;
+    private int views;
+    private int curtidas;
+    private boolean reproduzindo;
+
+    public Video(String titulo){
+        this.setTitulo(titulo);
+        this.setAvaliacao(1);
+        this.setViews(0);
+        this.setCurtidas(0);
+        this.setReproduzindo(false);
+        
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        
+        this.avaliacao = avaliacao;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public boolean isReproduzindo() {
+        return reproduzindo;
+    }
+
+    public void setReproduzindo(boolean reproduzindo) {
+        this.reproduzindo = reproduzindo;
+    }
+
+    @Override
+    public void play() {
+        this.setReproduzindo(true);
+    }
+
+    @Override
+    public void pause() {
+        this.setReproduzindo(false);
+    }
+
+    @Override
+    public void like() {
+        this.setCurtidas(getCurtidas()+1);
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" + "titulo=" + titulo + ", avaliacao=" + avaliacao + ", views=" + views + ", curtidas=" + curtidas + ", reproduzindo=" + reproduzindo + '}';
+    }
+    
+    
+}
